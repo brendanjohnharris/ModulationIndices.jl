1d3a7a1573eb4670a071c7dc6eb31346e2f4b9a7
brendanjohnharris <brendanjohnharris@gmail.com>
2022-09-15 10:26:11 +1000

    Add tort2010

--- src/ModulationIndices.jl
+++ src/ModulationIndices.jl
@@ -1,5 +1,18 @@
 module ModulationIndices
 
-# Write your package code here.
+using Catch22
+using DSP
+using Statistics
+using StatsBase
+
+export analytic_signal, tort2010
+
+analytic_signal = Feature(hilbert, :analytic_signal, "Analytic signal computed via the Hilbert transform", ["amplitude,phase"])
+
+bandpass(x::Vector, fs=1; pass) = filtfilt(digitalfilter(Bandpass(pass...; fs), Butterworth(4)), x)
+
+
+include("Indices/Tort2008.jl")
+
 
 end
